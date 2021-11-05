c1add0965022592646fb153f71e11743f9a7e58a
Adriano <adriano.halo@gmail.com>
2021-11-05 13:20:55 +0000

    Added rpg-portugal-logging

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,5 @@ include("rpg-portugal-core")
 include("rpg-portugal-configuration")
 include("rpg-portugal-orthanc")
 include("rpg-portugal-jobs")
+include("rpg-portugal-logging")
+findProject(":rpg-portugal-logging")?.name = "rpgportugal-logging"
